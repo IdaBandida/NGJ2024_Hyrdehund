48a1bba41fe7f895f6b1c5e8b98d5edd51c6193e
BJORN-VINTHER <g.bvinther@gmail.com>
2024-04-21 10:12:40 +0200

    fix collision

--- Assets/Scripts/SheepLogic.cs
+++ Assets/Scripts/SheepLogic.cs
@@ -1,24 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SheepLogic : MonoBehaviour
+public class SheepLogic
 {
     public float minRange = 0f;
     public float maxRange = 10f;
     public float minForce = 0f;
     public float maxForce = 10f;
 
-    public Vector2 GetAttraction(Vector2 other)
+    public Vector2 GetAttraction(Vector2 pos, Vector2 other)
     {
-        var delta = other - (Vector2)transform.position;
+        var delta = other - pos;
         if (delta.magnitude > maxRange || delta.magnitude < minRange)
         {
             return Vector2.zero;
         }
 
         var force = Mathf.Lerp(maxForce, minForce, (delta.magnitude - minRange) / maxRange);
-        //print(force);
         return delta.normalized * force;
     }
 }
